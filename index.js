222d64ccffbb781b04ed4aa6fa54ac5528ca1168
Aditi09-kothari <147057560+Aditi09-kothari@users.noreply.github.com>
2024-02-14 16:12:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://myapp-client.vercel.app",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
